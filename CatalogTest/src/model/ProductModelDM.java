f3a1ec1f1c3208a501b0a0ce361c5c57f6bd0d5b
oid96 <81259949+oid96@users.noreply.github.com>
2021-05-01 17:10:16 +0200

    Update ProductModelDM.java

--- CatalogTest/src/model/ProductModelDM.java
+++ CatalogTest/src/model/ProductModelDM.java
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 
 public class ProductModelDM implements ProductModel {
 
-	private static final String TABLE_NAME = "product";
+	private static final String TABLE_NAME = "prodotto";
 
 	@Override
 	public synchronized void doSave(ProductBean product) throws SQLException {
@@ -17,19 +17,24 @@ public class ProductModelDM implements ProductModel {
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 
-		String insertSQL = "INSERT INTO " + ProductModelDM.TABLE_NAME
-				+ " (NAME, DESCRIPTION, PRICE, QUANTITY) VALUES (?, ?, ?, ?)";
+		String insertSQL = "call print3d.inserisci_prodotto(?, ?, ?, ?, ?, ?, ?);";
 
 		try {
 			connection = DriverManagerConnectionPool.getConnection();
 			preparedStatement = connection.prepareStatement(insertSQL);
-			preparedStatement.setString(1, product.getName());
-			preparedStatement.setString(2, product.getDescription());
-			preparedStatement.setInt(3, product.getPrice());
-			preparedStatement.setInt(4, product.getQuantity());
-
+			preparedStatement.setInt(1, product.getCode());
+			preparedStatement.setString(2, product.getShortdesc());
+			preparedStatement.setString(3, product.getDescription());
+			if(product.getAvailable()) {
+				preparedStatement.setBit(4, 1);
+			}
+			else {
+				preparedStatement.setBit(4, 0);
+			}
+			preparedStatement.setDecimal(5, product.getVat());
+			preparedStatement.setDecimal(6, product.getBase_price());
+			preparedStatement.setString(7, product.getCategory());
 			preparedStatement.executeUpdate();
-
 			connection.commit();
 		} finally {
 			try {
@@ -53,16 +58,23 @@ public class ProductModelDM implements ProductModel {
 		try {
 			connection = DriverManagerConnectionPool.getConnection();
 			preparedStatement = connection.prepareStatement(selectSQL);
-			preparedStatement.setInt(1, code);
+			preparedStatement.setInt(1, codice);
 
 			ResultSet rs = preparedStatement.executeQuery();
 
 			while (rs.next()) {
-				bean.setCode(rs.getInt("CODE"));
-				bean.setName(rs.getString("NAME"));
-				bean.setDescription(rs.getString("DESCRIPTION"));
-				bean.setPrice(rs.getInt("PRICE"));
-				bean.setQuantity(rs.getInt("QUANTITY"));
+				bean.setCode(rs.getInt("id_prodotto"));
+				bean.setShortdesc(rs.getString("descrizione_breve"));
+				bean.setDescription(rs.getString("descrizione"));
+				bean.setBase_price(rs.getDecimal("prezzo_base"));
+				if(rs.getBit("disponibilita")==1) {
+					bean.setAvailable(true);
+				}
+				else {
+					bean.setAvailable(false);
+				}
+				bean.setVat(rs.getDecimal("iva"));
+				bean.setCategory(rs.getString("categoria"));
 			}
 
 		} finally {
@@ -83,7 +95,7 @@ public class ProductModelDM implements ProductModel {
 
 		int result = 0;
 
-		String deleteSQL = "DELETE FROM " + ProductModelDM.TABLE_NAME + " WHERE CODE = ?";
+		String deleteSQL = "DELETE FROM " + ProductModelDM.TABLE_NAME + " WHERE codice = ?";
 
 		try {
 			connection = DriverManagerConnectionPool.getConnection();
@@ -125,11 +137,18 @@ public class ProductModelDM implements ProductModel {
 			while (rs.next()) {
 				ProductBean bean = new ProductBean();
 
-				bean.setCode(rs.getInt("CODE"));
-				bean.setName(rs.getString("NAME"));
-				bean.setDescription(rs.getString("DESCRIPTION"));
-				bean.setPrice(rs.getInt("PRICE"));
-				bean.setQuantity(rs.getInt("QUANTITY"));
+				bean.setCode(rs.getInt("id_prodotto"));
+				bean.setShortdesc(rs.getString("descrizione_breve"));
+				bean.setDescription(rs.getString("descrizione"));
+				bean.setBase_price(rs.getDecimal("prezzo_base"));
+				if(rs.getBit("disponibilita")==1) {
+					bean.setAvailable(true);
+				}
+				else {
+					bean.setAvailable(false);
+				}
+				bean.setVat(rs.getDecimal("iva"));
+				bean.setCategory(rs.getString("categoria"));
 				products.add(bean);
 			}
 
